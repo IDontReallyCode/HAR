7c5166aab69b035235d4cfa361264ef1bf165e39
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2022-07-28 12:48:01 -0500

    basic setup

---
+++ HAR/HAR.py
@@ -0,0 +1,14 @@
+"""
+This library will receive a pandas Dataframe with intraday data for a time series
+Will "clean" up the data and regularize the time series aggregation
+Will calculate the daily realized volatility based on a specified time aggregation (5min default)
+Will return the 1-day, 1-week, 2-week, 1-month (and other) time agregation realized volatilities 
+Will estimate the HAR forecasting model
+"""
+
+import numpy as np
+import pandas as pd
+
+
+
+
