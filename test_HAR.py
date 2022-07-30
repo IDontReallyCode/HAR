6d304595923e0ce36f9ffbd90dc4b615d9b2758d
IDontReallyCode <92130644+IDontReallyCode@users.noreply.github.com>
2022-07-29 19:09:59 -0500

    Update ReadMe

--- test_HAR.py
+++ test_HAR.py
@@ -14,11 +14,14 @@ def main():
     rvdays, realizeddailyvariance = rv.rv(data)
 
     multiplesampling = rv.rvaggregate(realizeddailyvariance)
+    X = np.ones((np.size(multiplesampling,0)-1,np.size(multiplesampling,1)+1))
+    X[:,1:] = multiplesampling[0:-1,:]
+    y = multiplesampling[1:,0]
 
     # we need to create the X matrix that has a column of 1, then each time horizon RV
     # the y will be a the daily RV shifted by N days, depending on the time horizon we want to forecast
 
-    beta = np.linalg.lstsq(X,y)[0]
+    beta = np.linalg.lstsq(X,y,rcond=None)[0]
 
     # dates = pltd.date2num(rvdays)
     # # plt.plot_date(dates, np.sqrt(realizeddailyvariance*252), 'b-')
@@ -31,6 +34,8 @@ def main():
     # plt.legend()
     # plt.show()
 
+
+    done=1
 
 
 #### __name__ MAIN()
